5a88e7ed73d439464dc8344a5c05b89c07c7de0d
scalerone <676119236@qq.com>
2019-09-15 19:43:52 +0800

    定义组件和jsx语法

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+// import React from 'react';
 
-function App() {
-  return (
-    <div >
-      Hello React
-    </div>
-  );
+
+import React, {Component} from 'react';
+//定义一个组件
+class App extends Component {
+  render() {
+    //jsx语法  表达式  三元表达式
+    const  name ="你好"
+    const isLogin = true
+    return (
+        <div>
+          Hello React {1+2} {name}
+          {isLogin ? <h2>已登录</h2>:<h2>未登录</h2>}
+        </div>
+    );
+  }
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <div >
+//       Hello React
+//     </div>
+//   );
+// }
+//
+// export default App;
